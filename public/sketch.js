1d84d12740d13737bdc71b10cb7a5599ea825dd2
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2019-10-30 12:32:19 +0100

    added fserver and clients iles

---
+++ public/sketch.js
@@ -0,0 +1,42 @@
+function preload(){
+  // put preload code here
+}
+
+var socket;
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+
+//load the socket.io library
+  socket = io();
+
+//ogni volta che ricevi informazioni da mouseBroadcast fai qualcosa
+  socket.on('mouseBroadcast', newDrawing);
+//descrivo la funzione, cosa deve fare
+  function newDrawing(receiveData){
+    fill('yellow');
+    ellipse(receiveData.x, receiveData.y, 5);
+  }
+
+  background('aquamarine');
+  // put setup code here
+}
+
+function draw() {
+
+  // put drawing code here
+}
+
+function mouseDragged(){
+  fill('white');
+  ellipse(mouseX,mouseY, 10);
+
+  //creare var con informazioni da inviare al server
+  var sendData = {
+    x:mouseX,
+    y:mouseY
+  }
+
+  //inviare informazioni
+  socket.emit('mouse', sendData);
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+console.log('sei bravissima');
+
+//per richiamare express, con il reqiore òa variabile diventa una funzione
+var express = require('express');
+//per attivarlo
+var app = express();
+
+//il numero è quello di localhost:xxxx
+var port = 3000;
+//che cartella voglio usare
+app.use(express.static('public'));
+
+//x creare una nuova connessione
+var server = app.listen(port);
+
+//x vedere il mio link
+console.log('http://localhost'+port);
+
+//fare lo stesso di esxpress con socket
+//socket permette di inviare informazioni a tutti i clients
+var socket = require('socket.io');
+
+//scrivere un codice che verrà aggiornato ogni volta che si aggiornerà il server
+var io = socket(server);
+
+//quando un altro cliente è attaccato al server
+//quando c'è una nuova connessione fai partire la funzione new Connection
+//la nuova connessione è il cliente, ciè gli altri sketch
+io.on('connection', newConnection);
+
+function newConnection(socket){
+  console.log(socket.id);
+
+  //riceve informazioni da sketch che si chiama 'mouse'
+  socket.on('mouse', mouseMessage);
+  //creare funzione mouseMessage
+  function mouseMessage(receiveData){
+    console.log(receiveData);
+
+  //inviare dal server le informazioni a tutti gli altri clients ma non ha chi l'ha creato
+    socket.broadcast.emit('mouseBroadcast', receiveData);
+  }
+}
