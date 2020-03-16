46fe215daa3eb59cf0db9e7c954dbad7e1aab381
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2020-03-17 00:10:11 +0100

    red

--- public/sketch.js
+++ public/sketch.js
@@ -15,7 +15,7 @@ function setup() {
   var button = createButton('Play');
   button.mouseClicked(play);
 
-  var buttonNumber = createButton(number);
+  var buttonNumber = createButton(my_number);
   buttonNumber.mouseClicked(numberPiuUno);
 
 
@@ -90,7 +90,7 @@ function play(){
   var x_rect = random(0,width);
   var y_rect = random(0,height);
 
-  fill('orange');
+  fill('red');
   rect(x_rect, y_rect, 30, 30);
 
   var sendData = {
